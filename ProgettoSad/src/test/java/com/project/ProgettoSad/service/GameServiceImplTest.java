fa4a9154a98190c28d374e18e852060783e6a836
learena <letiziarena@gmail.com>
2023-06-13 16:56:34 +0200

    test

---
+++ ProgettoSad/src/test/java/com/project/ProgettoSad/service/GameServiceImplTest.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package com.project.ProgettoSad.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author letiz
+ *
+ */
+public class GameServiceImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for createGame
+	 */
+	@Test
+	public void testCreateGame() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.project.ProgettoSad.service.GameServiceImpl#endGame(org.bson.types.ObjectId, java.lang.String)}.
+	 */
+	@Test
+	public void testEndGame() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.project.ProgettoSad.service.GameServiceImpl#getAllGames()}.
+	 */
+	@Test
+	public void testGetAllGames() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.project.ProgettoSad.service.GameServiceImpl#readPlayerHistory(java.lang.String)}.
+	 */
+	@Test
+	public void testReadPlayerHistory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.project.ProgettoSad.service.GameServiceImpl#getGameById(org.bson.types.ObjectId)}.
+	 */
+	@Test
+	public void testGetGameById() {
+		fail("Not yet implemented");
+	}
+
+}
